3e0c3abfc28f588ec80ce8834bd797dbfc883cbc
Nabin Katwal <51050967+NabinKatwal@users.noreply.github.com>
2021-08-27 21:39:56 +0545

    header css

--- yt/src/Header.js
+++ yt/src/Header.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import './Header.css'
+import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import VideoCallIcon from "@material-ui/icons/VideoCall"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import { Avatar } from '@material-ui/core'
 
 function Header() {
+    const avatar_image = "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
     return (
         <div>
             <div className='header'>
-                <h1>Header</h1>
+                <div className="header__left">
+                    <MenuIcon />
+                    <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="" />
+                </div>
+                <div className="header__input">
+                    <input type="text"/>
+                    <SearchIcon className="header__inputButton" />
+                </div>
+                <div className="header__icons">
+                    <VideoCallIcon />
+                    <AppsIcon />
+                    <NotificationsIcon />
+                    <Avatar
+                        alt = "User"
+                        src = { avatar_image }
+                    />
+                </div>
             </div>
         </div>
     )
